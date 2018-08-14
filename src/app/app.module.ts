3116dd56d33f7d698c9e8d8a29fbcc15d2f5d9e9
Dimitri <couwenberg@gmail.com>
2018-08-14 15:10:15 +1000

    feat: done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { DingComponent } from './ding/ding.component';
+import { BongComponent } from './bong/bong.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DingComponent,
+    BongComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
