e9dc341b9debc4a92ce4f8d35389b19c95340c38
Dimitri <couwenberg@gmail.com>
2018-08-14 15:16:43 +1000

    feat: added herodetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { DingComponent } from './ding/ding.component';
 import { BongComponent } from './bong/bong.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     DingComponent,
-    BongComponent
+    BongComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,FormsModule
